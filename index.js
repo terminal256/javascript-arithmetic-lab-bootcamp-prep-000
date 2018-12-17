9f37d7656e3d40144e9f042be19dcbf626d85e22
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 01:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function add(a, b){return}
-(a, b){return}
-(a, b){return}
-(a, b){return}
-(a, b){return}
-(a, b){return}
+function substract (a, b){return}
+function multiply(a, b){return}
+function divide(a, b){return}
+function inc(a, b){return}
+function dec(a, b){return}
